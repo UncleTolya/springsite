c7f2515e27f7272c4c6bf88ea7382b928dc9c647
uncletolya <stolikzakazan@gmail.com>
2019-03-24 23:53:49 +0300

    add filter

--- src/main/java/com/mhlv/springsite/repos/MessagesRepo.java
+++ src/main/java/com/mhlv/springsite/repos/MessagesRepo.java
@@ -3,5 +3,8 @@ package com.mhlv.springsite.repos;
 import com.mhlv.springsite.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessagesRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String filter);
 }
